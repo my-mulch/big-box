e9535025f90d392de97e2f65078ea43c9298fc6d
Truman Purnell <truman.purnell@gmail.com>
2017-08-03 01:54:38 -0400

    feature: creates comment posing matrix as functional tool

--- lin-alg/matrix.js
+++ lin-alg/matrix.js
@@ -37,5 +37,32 @@ function zeros(rows, cols) {
     return Array(rows).fill(null).map(_ => [])
 }
 
+/**
+ * 
+ * Creates an identity matrix size rows x cols
+ * 
+ * @param {int} rows
+ * @param {int} cols
+ */
+function eye(rows, cols) {
+    return Array(rows).fill(null).map((_, x_i) => [
+        // spread syntax https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
+        ...Array(cols).fill(null).map((_, y_i) => (
+            x_i === y_i ? 1 : 0
+        ))
+    ])
+}
 
+`
+                                                            A matrix can be used to 
+                                                            transform a vector dim N to K
+                                                            and back
+                                                            
+[1,2,3,4,5]              K   
+                  [[1,2,3,4,5,6],                                   
+                   [1,2,3,4,5,6],
+         N         [1,2,3,4,5,6],       =   [15,30,45,60,75,90] 
+                   [1,2,3,4,5,6],
+                   [1,2,3,4,5,6]]
 
+`
