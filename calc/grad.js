37c87c1a2618e57c16ec1632de566f54e14d40bf
Truman Purnell <truman.purnell@gmail.com>
2017-08-04 13:08:24 -0400

    feature: adds gradient check

--- calc/grad.js
+++ calc/grad.js
@@ -6,13 +6,42 @@
 
 /**
  * 
+ * This function computes the approximate derivate of a function with respect to its sole input
+ * 
  * @param {function} f 
  * @param {float} x 
  * @param {float} h 
  * 
  */
-function difference_quotient(f,x,h){
+function dfdx(f, x, h = 0.00001) {
     return (f(x + h) - f(x)) / h
 }
 
-console.log(difference_quotient((x) => x * x, 10, 0.000001))
+/**
+ * 
+ * This function computes the derivative of our function f with respect to index j of our vector v
+ * 
+ * @param {function} f 
+ * @param {vector} v 
+ * @param {int} j 
+ * @param {float} h 
+ *  
+ */
+function dfdj(f, v, j, h) {
+    const dj = v.map((v_i, i) => i === j ? v_i + h : v_i)
+    return (f(dj) - f(v)) / h
+}
+
+
+/**
+ * 
+ * Computes the gradient of a function (the partial derivative along each dimension of input vector)
+ * 
+ * @param {function} f 
+ * @param {vector} v 
+ * @param {float} 
+ *  
+ */
+function grad_est(f, v, h = 0.00001) {
+    return v.map((_, j) => dfdj(f, v, j, h))
+}
