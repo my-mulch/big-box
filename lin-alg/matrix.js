965902f90c5ce7ae410e1e5f4b4cc6e25ce73e44
Truman Purnell <truman.purnell@gmail.com>
2017-08-05 13:39:08 -0400

    chore: flips file names to factor out commonalities between matrix & vector

--- lin-alg/matrix.js
+++ lin-alg/matrix.js
@@ -1,67 +1,18 @@
 /* 
-
-        These methods provide basic operations for matrices
-
+   
+           These methods provide basic operations for matrices
+   
 */
+
+const vector = require('./vector')
+const ndarray = require('./transpose')
 
 module.exports = {
 
-    /*     Dependencies     */
-    vector: require('./vector'),
-
     /**
-     * 
-     * Returns the shape of a given matrix
-     * @param {matrix} A
-     * 
-     */
-
-    shape: function (A) {
-        return [A.length, A[0].length]
-    },
-
-    /**
-     * 
-     * Returns the column for a given matrix
-     * 
-     * @param {matrix} A
-     * @param {int} j 
-     */
-    column: function (A, i) {
-        return A.map(a => a[i])
-    },
-
-    /**
-     * 
-     * Creates a rows x cols matrix of zeros
-     * 
-     * @param {int} rows
-     * @param {int} cols
-     */
-    zeros: function (rows, cols) {
-        return Array(rows).fill(null).map(_ => Array(cols).fill(0))
-    },
-
-    /**
-     * 
-     * Creates an identity matrix size rows x cols
-     * 
-     * @param {int} rows
-     * @param {int} cols
-     */
-    eye: function (rows, cols) {
-        return Array(rows).fill(null).map((_, i) => [
-            // spread syntax https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
-            ...Array(cols).fill(null).map((_, j) => (
-                i === j ? 1 : 0
-            ))
-        ])
-    },
-
-    /**
-     *                                                           A matrix can be used to 
-     *                                                           transform a vector dim N to K
-     *                                                           and back                                                          
+     *                                            A matrix can be used to 
+     *                                            transform a vector dim N to K
+     *                                            and back                                                          
      *  [1,2,3,4,5]          K   
      *                 [[1,2,3,4,5,6],                                   
      *                  [1,2,3,4,5,6],
@@ -74,9 +25,10 @@ module.exports = {
      * @param {matrix} A
      * @param {matrix} B
      */
-    dot: function (A, B) {
-        return A.map((row, i) => {
-            return B[0].map((_, j) => vector.dot(column(B, j), row))
-        })
-    },
+    dot(A,B){
+
+    }
 }
+
+
+
--- lin-alg/vector.js
+++ lin-alg/vector.js
@@ -1,92 +1,10 @@
 /* 
-   
-           These methods provide basic operations for matrices
-   
+
+        These methods provide basic operations for vectors
+
 */
-function elementwise(a, b, f, c = []) {
-    for (let i = 0; i < a.length; i++)
-        // null for elementwise over only one matrix
-        if (Array.isArray(a[i]))
-            elementwise(a[i], b ? b[i] : null, f)
-        else c[i] = f(a[i], b ? b[i] : null)
-    return c
-}
-
-function norm(m) {
-    const accumulator = (m_i) => accumulator.total += m_i * m_i
-    accumulator.total = 0
-    elementwise(m, null, accumulator)
-    console.log(Math.sqrt(accumulator.total))
-}
-
-
-
-
-const start = new Date().getTime()
-norm([1, 2, 3])
-const end = new Date().getTime()
-console.log((end - start) / 1000)
-
 
 module.exports = {
-
-    /**
-     * 
-     * Computes f elementwise on two matrices
-     * 
-     * @param {any} a input matrix
-     * @param {any} b input matrix
-     * @param {matrix} c result matrix of same size as inputs or empty
-     * @param {function} f function to apply 
-     * @returns {matrix} matrix with f applied elementwise
-     */
-
-    elementwise(a, b, f, c = []) {
-        for (let i = 0; i < a.length; i++)
-            // null for elementwise over only one matrix
-            if (Array.isArray(a[i]))
-                elementwise(a[i], b ? b[i] : null, f)
-            else c[i] = f(a[i], b ? b[i] : null)
-        return c
-    },
-
-    /**
-     * 
-     * Computes elementwise addition on two matrices
-     * 
-     * @param {matrix} a input matrix
-     * @param {matrix} b input matrix
-     * @returns {matrix}
-     */
-
-    add(a, b) {
-        return this.elementwise(a, b, (a_i, b_i) => a_i + b_i)
-    },
-
-    /**
-     * 
-     * Computes elementwise subtraction on two matrices
-     * 
-     * @param {matrix} a input matrix
-     * @param {matrix} b input matrix
-     * @returns {matrix}
-     */
-
-    sub(a, b) {
-        return this.elementwise(a, b, (a_i, b_i) => a_i - b_i)
-    },
-
-    /**
-     * 
-     * Mulitplies input matrix by a scalar
-     * 
-     * @param {matrix} m
-     * @param {scalar} c 
-     * @returns {matrix} 
-     */
-    scale(m, c) {
-        return this.elementwise(m, null, (m_i) => m_i * c)
-    },
 
     /**
      * 
@@ -96,57 +14,10 @@ module.exports = {
      * @returns {scalar} The dot product
      */
     dot(a, b) {
-        // return this.norm(a) * this.norm(b) * 
-    },
-
-    /**
-     * 
-     * Computes the squares of a vector elementwise
-     * @param {matrix} m 
-     * @returns {matrix} 
-     */
-    square(m) {
-        return this.elementwise(m, null, (m_i) => m_i * m_i)
-    },
-
-    /**
-     * 
-     * Computes frobenius norm of a matrix, L2 of vector
-     * @param {matrix} m
-     * @returns {matrix}
-     */
-    norm(m) {
-        // the accumulator heads through each element and gathers squares
-        const accumulator = (m_i) => accumulator.total += m_i * m_i
+        const accumulator = (a_i, b_i) => accumulator.total += a_i * b_i
         accumulator.total = 0
-        this.elementwise(m, null, accumulator)
-        // we take the root of these accumulated squares
-        // such is the definition of the frobenius
-        return Math.sqrt(accumulator.total)
+        this.elementwise(a, b, accumulator)
+        return accumulator.total
     },
-
-    /**
-     * 
-     * Computes the direction of a vector
-     * @param {vector} v 
-     * 
-     */
-    direction(v) {
-        const m = this.magnitude(v)
-        return v.map(v_i => v_i / m)
-    },
-
-    /**
-     * 
-     * Computes the distance between two vectors
-     * @param {vector} a
-     * @param {vector} b 
-     * 
-     */
-    distance(a, b) {
-        return this.magnitude(this.subtract(a, b))
-    }
+    
 }
-
-
-
