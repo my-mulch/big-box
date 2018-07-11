0164461aef070c8af95a0d5ada77e9ecd83db5e7
Truman Purnell <truman.purnell@gmail.com>
2018-07-10 19:58:37 -0700

    adds getData function to utils to access data in MultiDimArray

--- src/utils/array/ndim/index.js
+++ src/utils/array/ndim/index.js
@@ -27,12 +27,18 @@ export function helperToString(header, index = []) {
 }
 
 
-export function getSlice(index, oldHeader) {
+export function getData(indices, data, header) {
+    return header.offset + data[indices.reduce(function (finalIndex, idxValue, i) {
+        return finalIndex + idxValue * header.stride[i]
+    }, 0)]
+}
+
+export function getSlice(indices, oldHeader) {
     const newHeader = JSON.parse(JSON.stringify(oldHeader))
 
     for (let i = 0, del = 0; i < oldHeader.shape.length; i++) {
-        if (index[i] >= 0) {
-            newHeader.offset += oldHeader.stride[i] * index[i]
+        if (indices[i] >= 0) {
+            newHeader.offset += oldHeader.stride[i] * indices[i]
             newHeader.stride.splice(i - del, 1)
             newHeader.shape.splice(i - del, 1)
             del++
