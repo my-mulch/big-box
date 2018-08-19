3247f8fbbc76e8be7a0e8658b5a05f2a9b9adff4
Truman Purnell <truman.purnell@gmail.com>
2018-08-19 18:14:03 -0500

    moves raw array utils into separate file

---
+++ src/utils/raw.mjs
@@ -0,0 +1,38 @@
+export default class RawArrayUtils {
+    static * flatten(rawArray) {
+        for (let i = 0; i < rawArray.length; i++) {
+            if (Array.isArray(rawArray[i]))
+                yield* RawArrayUtils.flatten(rawArray[i])
+            else
+                yield rawArray[i]
+        }
+    }
+
+    static slice(rawArray, index) {
+        if (!index.length) return rawArray
+
+        return RawArrayUtils.slice(rawArray[index[0]], index.slice(1))
+    }
+
+    static createRawArray(shape, fill = () => 0) {
+        if (!shape.length) return fill()
+
+        return new Array(shape[0]).fill(null).map(function () {
+            return RawArrayUtils.createRawArray(shape.slice(1), fill)
+        })
+    }
+
+    static cycle(rawArray, n) {
+        const copy = [...rawArray]
+        return copy.splice(-n % copy.length).concat(copy)
+    }
+
+    static insert(rawArray, index, value) {
+        if (index.length === 1) {
+            rawArray[index[0]] = value
+            return
+        }
+
+        RawArrayUtils.insert(rawArray[index[0]], index.slice(1), value)
+    }
+}
