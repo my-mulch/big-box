a322513d66ba113976ae9e39e84abbeb583669fe
Truman Purnell <truman.purnell@gmail.com>
2018-09-08 23:44:02 -0700

    fixes non-return bug in squaring scalar method

--- src/math/scalar/index.mjs
+++ src/math/scalar/index.mjs
@@ -5,5 +5,5 @@ export default class ScalarOperator {
     static subtract(a, b) { return a - b }
     static min(a, b) { return Math.min(a, b) }
     static max(a, b) { return Math.max(a, b) }
-    static square(a) { a * a }
+    static square(a) { return a * a }
 }
