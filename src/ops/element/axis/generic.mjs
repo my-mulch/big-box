400b6f9cebf082002070948abf82a30ae01464f3
Truman Purnell <truman.purnell@gmail.com>
2019-02-05 23:32:18 -0800

    suite passing with tidier generic axis suite

--- src/ops/element/axis/generic.mjs
+++ src/ops/element/axis/generic.mjs
@@ -1,34 +1,30 @@
 export default function (args) {
-    const source = [], rIndex = [], aIndex = [], rl = args.axes[1].length, al = args.axes[0].length
-
-    source.push('let ri = 0, ai = 0, ii = 0')
+    const rIndex = [], rLoops = [], aIndex = [], aLoops = [],
+        rl = args.axes[1].length, al = args.axes[0].length
 
     for (let i = 0; i < rl; i++) {
-        source.push(`for(let r${i} = 0; r${i} < args.of.header.shape[args.axes[1][${i}]]; r${i}++){`)
-        rIndex.push(`r${i} * args.of.header.strides[args.axes[1][${i}]]`)
+        rLoops.push(`for(let r${i} = 0; r${i} < args.of.header.shape[args.axes[1][${i}]]; r${i}++){`)
+        aIndex.push(`r${i} * args.of.header.strides[args.axes[1][${i}]]`)
+        rIndex.push(`r${i} * args.result.header.strides[${i}]`)
     }
 
-    source.push('ii = 0')
-
     for (let i = 0; i < al; i++) {
-        source.push(`for(let a${i} = 0; a${i} < args.of.header.shape[args.axes[0][${i}]]; a${i}++){`)
+        aLoops.push(`for(let a${i} = 0; a${i} < args.of.header.shape[args.axes[0][${i}]]; a${i}++){`)
         aIndex.push(`a${i} * args.of.header.strides[args.axes[0][${i}]]`)
     }
 
-    source.push(
+
+    return new Function('args', [
+        `let ri = 0, ai = 0, ii = 0`,
+        ...rLoops,
+        'ii = 0',
+        ...aLoops,
         `ai = args.of.header.offset + ${aIndex.join('+') || 0}`,
         `ri = args.result.header.offset + ${rIndex.join('+') || 0}`,
-        `if(!ii) { args.result.data[ri] = args.of.data[ai] } ii++`
-    )
-
-    const mdi = `[${'a'.repeat(rl + al).split('').map(function (a, i) { return a + i }).join(',')}]`
-
-    source.push(
-        `args.result.data[ri] = args.reducer(args.mapper(args.of.data[ai], ai, ${mdi}), args.result.data[ri], ii)`,
+        `if(!ii) { args.result.data[ri] = args.of.data[ai] } ii++`,
+        `args.result.data[ri] = args.reducer(args.mapper(args.of.data[ai]), args.result.data[ri], ii)`,
         '}'.repeat(al),
         '}'.repeat(rl),
         `return args.result`
-    )
-
-    return new Function('args', source.join('\n'))
+    ].join('\n'))
 }
