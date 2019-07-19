640614460471502ef2c9b2deed9a21eb95f596da
trumanpurnell <truman.purnell@gmail.com>
2019-07-19 11:19:58 -0700

    adds node style module resolution in package json

--- src/test/index.mjs
+++ src/test/index.mjs
@@ -2,9 +2,7 @@ import creation from './suites/creation'
 import linalg from './suites/linalg'
 import element from './suites/element'
 import indexing from './suites/indexing'
-import speed from './suites/speed'
 
-speed.call()
 creation.call()
 linalg.call()
 element.call()
